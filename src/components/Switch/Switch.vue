e6806e71a83e31a7eab521f0c8dd52acc22f127c
liang <2528726609@qq.com>
2025-02-27 19:03:56 +0800

    switch

--- src/components/Switch/Switch.vue
+++ src/components/Switch/Switch.vue
@@ -1,44 +1,66 @@
 <template>
-  <div
-    class="vk-switch"
-    :class="{
-      [`vk-switch--${size}`]: size,
-      'is-diabled': disabled,
-      'is-checked': checked,
-    }"
-     @click="switchValue"
-  >
-  <input
-  class="vk-switch__input"
-   type="checkbox"
-   role="switch"
-   :name="name"
-   :disabled="disabled">
-   <div class="vk-switch__core">
-    <div class="vk-switch__core-action"></div>
-   </div>
+<div
+  class="vk-switch"
+  :class="{
+    [`vk-switch--${size}`]: size,
+    'is-disabled': disabled,
+    'is-checked': checked
+  }"
+  @click="switchValue"
+>
+  <input 
+    class="vk-switch__input"
+    type="checkbox"
+    role="switch"
+    ref="input"
+    :name="name"
+    :disabled="disabled"
+    @keydown.enter="switchValue"
+  />
+  <div class="vk-switch__core">
+    <div class="vk-switch__core-inner">
+      <span v-if="activeText || inactiveText" class="vk-switch__core-inner-text">
+        {{checked ? activeText : inactiveText}}
+      </span>
+    </div>
+    <div class="vk-switch__core-action">
+    </div>
   </div>
+</div>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from "vue";
-import type { SwtichEmits, SwtichProps } from "./types";
+import { ref, computed, onMounted, watch } from 'vue'
+import type { SwitchProps, SwitchEmits } from './types'
 
 defineOptions({
-  name: "VkSwitch",
-});
+  name: 'VkSwtich',
+  inheritAttrs: false
+})
+const props = withDefaults(defineProps<SwitchProps>(), {
+  activeValue: true,
+  inactiveValue: false
+})
+const emits = defineEmits<SwitchEmits>()
 
-const props = defineProps<SwtichProps>();
-const emits = defineEmits<SwtichEmits>();
-
-const innerValue = ref(props.modelValue);
-const checked = computed(() => innerValue.value);
-const switchValue = () =>{
-    if(props.disabled) return
-    innerValue.value = !checked.value
-    emits('update:modelValue',innerValue.value)
-    emits('change',innerValue.value)
+const innerValue = ref(props.modelValue)
+const input = ref<HTMLInputElement>()
+// 现在是否被选中
+const checked = computed(() => innerValue.value === props.activeValue)
+const switchValue = () => {
+  if (props.disabled) return
+  const newValue = checked.value ? props.inactiveValue : props.activeValue
+  innerValue.value = newValue
+  emits('update:modelValue', newValue)
+  emits('change', newValue)
 }
+onMounted(() => {
+  input.value!.checked = checked.value
+})
+watch(checked, (val) => {
+  input.value!.checked = val 
+})
+watch(() => props.modelValue, (newValue) => {
+  innerValue.value = newValue
+})
 </script>
-
-<style></style>
--- src/components/Switch/types.ts
+++ src/components/Switch/types.ts
@@ -1,17 +1,17 @@
 export type SwitchValueType = boolean | string | number;
-export interface SwtichProps {
-  modelValue: boolean;
+export interface SwitchProps {
+  modelValue: SwitchValueType;
   disabled?: boolean;
   activeText?: string;
   inactiveText?: string;
-  activeValue?: string;
-  inactiveValue?: string;
+  activeValue?: SwitchValueType;
+  inactiveValue?: SwitchValueType;
   name?: string;
   id?: string;
   size?: "small" | "large";
 }
 
-export interface SwtichEmits {
-  (e: "update:modelValue", value: boolean): void;
-  (e: "change", value: boolean): void;
+export interface SwitchEmits {
+  (e: "update:modelValue", value: SwitchValueType): void;
+  (e: "change", value: SwitchValueType): void;
 }
